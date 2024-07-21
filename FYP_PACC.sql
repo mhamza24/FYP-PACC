fbbcb406a9f03a228249973d9505efb95c255681
mhamza24 <muhammadhamzakhalid24@gmail.com>
2024-07-21 23:03:23 +0500

    database-file added

---
+++ FYP_PACC.sql
@@ -0,0 +1,41 @@
+create database face_detection;
+use face_detection;
+
+-- drop database face_detection;
+CREATE TABLE traindata (
+    auto_id INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(255),
+    ID VARCHAR(50),
+    Section VARCHAR(50),
+    Type ENUM('staff','student'),
+    department VARCHAR(255)
+);
+
+CREATE TABLE staff_slips (
+    slip_id INT AUTO_INCREMENT PRIMARY KEY,
+    staff_id VARCHAR(50),
+    staff_name VARCHAR(255),
+    department VARCHAR(255),
+    reason TEXT,
+    duration varchar(255) NOT NULL,
+    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+
+CREATE TABLE fine (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  student_id VARCHAR(255) NOT NULL,
+ student_name VARCHAR(255) NOT NULL,
+ department varchar(255),
+  fine_type VARCHAR(255) NOT NULL,
+  fine_amount INT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+select * from traindata;
+
+
